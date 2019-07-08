223f0e098d938fc8a49c8467ca1c06939b1ce4d0
annisamonitha <annisamonitha12@gmail.com>
2019-07-08 13:38:04 +0700

    pklkita

---
+++ CRUDANGGOTA/Migrations/20190705050112_Intial Create.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUDANGGOTA.Migrations
+{
+    public partial class IntialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Anggotas",
+                columns: table => new
+                {
+                    AnggotaID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nama = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Nim = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Jabatan = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Anggotas", x => x.AnggotaID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Anggotas");
+        }
+    }
+}
