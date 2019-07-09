fdd469489f582aef5dafb4f147361ff8e4f227e3
unknown <safirah988@gmail.com>
2019-07-09 10:17:09 +0700

    Daftaranggota dengan frontend adminLTE

---
+++ adminLTE/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace adminLTE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Beranda()
+        {
+            return View();
+        }
+        public IActionResult Daftaranggota()
+        {
+            return View();
+        }
+        public IActionResult Daftarprestasi()
+        {
+            return View();
+        }
+        public IActionResult Pengarsipan()
+        {
+            return View();
+        }
+        public IActionResult Publikasi()
+        {
+            return View();
+        }
+        public IActionResult PreviewPublikasi()
+        {
+            return View();
+        }
+        public IActionResult KalenderKegiatan()
+        {
+            return View();
+        }
+
+    }
+}
